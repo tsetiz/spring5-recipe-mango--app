1030b276bdabc952993e6ecd493fb3d3a06b2b81
kshitizbista <tsetiz.bista@gmail.com>
2018-09-24 00:43:44 +0545

    Added bootstrap for database initialization

--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -22,4 +22,22 @@ public class Notes {
     public void setId(Long id) {
         this.id = id;
     }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public String getRecipeNotes() {
+        return recipeNotes;
+    }
+
+    public void setRecipeNotes(String recipeNotes) {
+        this.recipeNotes = recipeNotes;
+    }
 }
+
+
--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -124,6 +124,7 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
     }
 
     public Set<Ingredient> getIngredients() {
@@ -132,6 +133,12 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Recipe addIngredient(Ingredient ingredient) {
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public Set<Category> getCategory() {
